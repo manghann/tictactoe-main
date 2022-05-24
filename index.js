d79b7caa1b1ce64787f0def1422e0ac666bb2737
Hanna Mangampo <mangampohannamae@gmail.com>
2022-05-24 17:06:14 +0800

    Add footer authors

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ window.addEventListener('DOMContentLoaded', () => {
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer');
 
-    let board = ['', '', '', '', '', '', '', '', ''];
+    let board = ['', '', '',
+                 '', '', '',
+                 '', '', ''];
     let currentPlayer = 'X';
     let isGameActive = true;
 
     const PLAYERX_WON = 'PLAYERX_WON';
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
-
 
     /*
         Indexes within the board
